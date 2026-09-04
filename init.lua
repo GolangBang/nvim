c8cd6da6d59741715da1e27238aefb182743dac2
Jake B <jakeb@Jakes-Mac-mini.local>
2026-09-03 22:55:35 -0700

    Added suggestions for Go on gopls using blink.cmp

--- init.lua
+++ init.lua
@@ -47,6 +47,39 @@ require("lazy").setup({
   { "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" } },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "neovim/nvim-lspconfig" },
+  {
+  "saghen/blink.cmp",
+  version = "*",
+  opts = {
+    keymap = {
+      preset = "default",
+    },
+
+    sources = {
+      default = {
+        "lsp",
+        "path",
+        "snippets",
+        "buffer",
+      },
+    },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 300,
+      },
+
+      ghost_text = {
+        enabled = true,
+      },
+    },
+
+    signature = {
+      enabled = true,
+    },
+  },
+},
 
 }, {
   ui = { border = "rounded" },
